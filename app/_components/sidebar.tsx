2ff99f1a91e400cabedca706326dd41478cf9cdf
abvictor <victor.amaralb@hotmail.com>
2024-12-22 13:05:18 -0300

    chore: initial setup

---
+++ app/_components/sidebar.tsx
@@ -0,0 +1,20 @@
+const Sidebar = () => {
+  return (
+    <aside className="w-64 bg-white">
+      {/* imagem aqui */}
+      <div></div>
+
+      <div className="px-8 py-6">
+        <h1 className="text-2xl font-bold">STOKING</h1>
+      </div>
+
+      <div className="flex flex-col gap-2 p-2">
+        <button className="px-6 py-3">Dashboard</button>
+        <button className="px-6 py-3">Produtos</button>
+        <button className="px-6 py-3">Vendas</button>
+      </div>
+    </aside>
+  );
+};
+
+export default Sidebar;
