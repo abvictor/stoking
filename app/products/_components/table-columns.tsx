91e67ce11a3b48b5627cb29fe24234f26cbcc4fc
abvictor <victor.amaralb@hotmail.com>
2024-12-28 16:04:09 -0300

    feat: adding actions button dropdown

--- app/products/_components/table-columns.tsx
+++ app/products/_components/table-columns.tsx
@@ -2,7 +2,17 @@
 import { Badge } from "@/app/_components/ui/badge";
 import { Product } from "@prisma/client";
 import { ColumnDef } from "@tanstack/react-table";
-import { Circle, CircleIcon } from "lucide-react";
+
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/app/_components/ui/dropdown-menu"
+import { Button } from "@/app/_components/ui/button";
+import { ClipboardCopyIcon, EditIcon, MoreHorizontalIcon, Trash2Icon } from "lucide-react";
 
 const getStatusLabel = (status: string) => {
     if(status === "IN_STOCK"){
@@ -29,16 +39,50 @@ export const productTableColumns: ColumnDef<Product>[] = [
     accessorKey: "status",
     header: "Status",
     cell: (row) => {
-        const product = row.row.original;
-        const label = getStatusLabel(product.status);
-        return (
-          <Badge
-            variant="default"
-            className={label === "Em estoque" ? "bg-green-500" : "bg-red-500"}
-          >
-            {label}
-          </Badge>
-        );
+      const product = row.row.original;
+      const label = getStatusLabel(product.status);
+      return (
+        <Badge
+          variant="default"
+          className={label === "Em estoque" ? "bg-green-500" : "bg-red-500"}
+        >
+          {label}
+        </Badge>
+      );
+    },
+  },
+  {
+    accessorKey: "actions",
+    header: "Ações",
+    cell: (row) => {
+      const product = row.row.original;
+      return (
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button variant="ghost">
+              <MoreHorizontalIcon size={16} />
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent>
+            <DropdownMenuItem
+              className="gap 1.5"
+              onClick={() => navigator.clipboard.writeText(String(product.id))}
+            >
+              <ClipboardCopyIcon size={16} />
+              Copiar ID
+            </DropdownMenuItem>
+            <DropdownMenuItem className="gap 1.5">
+              <EditIcon size={16} />
+              Editar produto
+            </DropdownMenuItem>
+            <DropdownMenuItem className="gap 1.5">
+              <Trash2Icon size={16}/>
+              Deletar produto
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      );
     }
   },
 ];
+
