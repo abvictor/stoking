086d8f2b391d6487c95665eb8c23ec1d6a807a5e
abvictor <victor.amaralb@hotmail.com>
2025-01-14 22:19:25 -0300

    feat: add DTO to product data-access

--- app/_data-access/product/get-product.ts
+++ app/_data-access/product/get-product.ts
@@ -1,14 +1,20 @@
-import "server-only"
+import "server-only";
 
 import { db } from "@/app/_lib/prisma";
 import { Product } from "@prisma/client";
-import { unstable_cache } from "next/cache";
 
-export const getProducts = async (): Promise<Product[]> => {
-    return db.product.findMany({})
+export type ProductStatusDto = "IN_STOCK" | "OUT_OF_STOCK";
+
+export interface ProductDto extends Omit<Product, "price"> {
+  price: number;
+  status: ProductStatusDto;
 }
 
-export const cachedGetProducts = unstable_cache(getProducts, ['getProducts'], {
-    tags: ["get-products"],
-    revalidate: 60
-})
+export const getProducts = async (): Promise<ProductDto[]> => {
+  const products = await db.product.findMany({});
+  return products.map((product) => ({
+    ...product,
+    price: Number(product.price),
+    status: product.stock > 0 ? "IN_STOCK" : "OUT_OF_STOCK",
+  }));
+};
